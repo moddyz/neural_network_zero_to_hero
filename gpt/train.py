48decc55b9754ea3408b64b1a1ac6ed73020b787
moddyz <moddy3d@gmail.com>
2023-12-27 19:07:58 +1300

    gpt wrapped up

--- gpt/train.py
+++ gpt/train.py
@@ -44,11 +44,15 @@ def main():
 
     # Set up our model's hyper parameters.
     hyper_params = HyperParameters(
-        batch_size=32,
-        block_size=8,
-        train_iters=10000,
+        batch_size=64,
+        block_size=256,
+        embed_size=384,
+        num_heads=6,
+        num_block_layers=6,
+        train_iters=5000,
         eval_iters=300,
-        learning_rate=1e-2,
+        learning_rate=3e-4,
+        dropout_prob=0.2,
         device=get_optimal_device(),
     )
 
@@ -57,7 +61,7 @@ def main():
     )
 
     # Create the model
-    model = BigramLanguageModel(vocab_size, hyper_params.device).to(hyper_params.device)
+    model = BigramLanguageModel(vocab_size, hyper_params).to(hyper_params.device)
 
     # Create an Adam optimizer
     optimizer = torch.optim.AdamW(model.parameters(), lr=hyper_params.learning_rate)
@@ -89,7 +93,7 @@ def main():
 
     # Generate some data
     inputs = torch.zeros((1, 1), dtype=torch.long).to(hyper_params.device)
-    predictions = model.generate(inputs, 400)
+    predictions = model.generate(inputs, 400, hyper_params)
     int_chars = predictions[0].tolist()
     chars = decode(int_chars)
     string = "".join(chars)
@@ -101,23 +105,134 @@ HyperParameters = collections.namedtuple(
     [
         "batch_size",  # Number of character sequences processed in parallel
         "block_size",  # Block size is the maximum context length to make predictions from.
+        "embed_size", # Size of a embeddding vector representing a single character.
+        "num_heads", # Number of self attention heads
+        "num_block_layers", # Number of transformer block layers
         "train_iters",  # Number of iterations to train the model.
         "eval_iters",  # Number of iterations for estimating loss.
         "learning_rate",  # The learning rate
+        "dropout_prob",  # The learning rate
         "device",  # The device to store and execute our neutral network.
     ],
 )
 
+class Block(nn.Module):
+
+    def __init__(self, hyper_params):
+        super().__init__()
+
+        head_size = hyper_params.embed_size // hyper_params.num_heads
+        self.self_attention_head = MultiHeadAttention(hyper_params)
+        self.feed_forward = FeedForward(hyper_params)
+        self.layer_norm_1 = nn.LayerNorm(hyper_params.embed_size)
+        self.layer_norm_2 = nn.LayerNorm(hyper_params.embed_size)
+
+    def forward(self, x):
+        x = self.layer_norm_1(x)
+        x = x + self.self_attention_head(x)
+        x = self.layer_norm_2(x)
+        x = x + self.feed_forward(x)
+        return x
+
+
+class Head(nn.Module):
+
+    def __init__(self, hyper_params):
+        super().__init__()
+        head_size = hyper_params.embed_size // hyper_params.num_heads
+        self.key = nn.Linear(hyper_params.embed_size, head_size, bias=False).to(hyper_params.device)
+        self.query = nn.Linear(hyper_params.embed_size, head_size, bias=False).to(hyper_params.device)
+        self.value = nn.Linear(hyper_params.embed_size, head_size, bias=False).to(hyper_params.device)
+        self.register_buffer("tril", torch.tril(torch.ones(hyper_params.block_size, hyper_params.block_size)))
+        self.dropout = nn.Dropout(hyper_params.dropout_prob)
+
+    def forward(self, inputs):
+        batch_size, block_size, embed_size = inputs.shape
+
+        # Keys are the input elements to compare against
+        key = self.key(inputs)
+
+        # Query represents the current element
+        query = self.query(inputs)
+
+        # Compute attention scores
+        head_size = key.shape[2]
+        wei = query @ key.transpose(-2, -1) * (head_size**-0.5)
+        wei = wei.masked_fill(self.tril[:block_size, :block_size] == 0, float("-inf")) # replace 0's with negative infinities
+        wei = F.softmax(wei, dim=-1) # normalize into probabilities with softmax
+
+        wei = self.dropout(wei)
+
+        # Weighted aggregation
+        value = self.value(inputs)
+        output = wei @ value
+
+        return output
+
+
+class FeedForward(nn.Module):
+
+    def __init__(self, hyper_params):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(hyper_params.embed_size, 4 * hyper_params.embed_size),
+            nn.ReLU(),
+            nn.Linear(4 * hyper_params.embed_size, hyper_params.embed_size),
+            nn.Dropout(hyper_params.dropout_prob),
+        )
+
+    def forward(self, inputs):
+        return self.net(inputs)
+
+
+class MultiHeadAttention(nn.Module):
+
+    def __init__(self, hyper_params):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(hyper_params) for _ in range(hyper_params.num_heads)])
+        self.proj = nn.Linear(hyper_params.embed_size, hyper_params.embed_size)
+        self.dropout = nn.Dropout(hyper_params.dropout_prob)
+
+    def forward(self, inputs):
+        out = torch.cat([head(inputs) for head in self.heads], dim=-1)
+        out = self.proj(out)
+        out = self.dropout(out)
+        return out
+
 
 class BigramLanguageModel(nn.Module):
-    def __init__(self, vocab_size, device):
+    def __init__(self, vocab_size, hyper_params):
         super().__init__()
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size).to(device)
+
+        self.token_embedding_table = nn.Embedding(
+            vocab_size, hyper_params.embed_size
+        ).to(hyper_params.device)
+
+        self.position_embedding_table = nn.Embedding(
+            hyper_params.block_size, hyper_params.embed_size
+        ).to(hyper_params.device)
+
+        self.blocks = nn.Sequential(*[Block(hyper_params) for _ in range(hyper_params.num_block_layers)])
+        self.layer_norm = nn.LayerNorm(hyper_params.embed_size)
+        self.linear_head = nn.Linear(hyper_params.embed_size, vocab_size)
 
     def forward(self, inputs, targets=None):
+        B, T = inputs.shape
+
         # This extracts a tensor with shape (Batch Size, Block Size, Vocab Size)
-        # Where each integer from inputs corresponds to a vocab-sized embedding vector.
-        logits = self.token_embedding_table(inputs)
+        # Where each integer from inputs corresponds to a embedding vector.
+        token_emb = self.token_embedding_table(inputs)  # B, T, embed_size
+        position_emb = self.position_embedding_table(torch.arange(T).to(get_optimal_device())) # T, C
+
+        combined_emb = token_emb + position_emb
+
+        # Apply self attention head.
+        attended_emb = self.blocks(combined_emb)
+
+        norm_emb = self.layer_norm(attended_emb)
+
+        logits = self.linear_head(norm_emb)  # (B, T, vocab_size)
 
         if targets is None:
             # During generation we do not need to compute loss.
@@ -131,10 +246,13 @@ class BigramLanguageModel(nn.Module):
 
         return logits, loss
 
-    def generate(self, inputs, max_new_tokens):
+    def generate(self, inputs, max_new_tokens, hyper_params):
         for _ in range(max_new_tokens):
+            # Crop input to last block_size tokens for making predictions
+            inputs_cropped = inputs[:, -hyper_params.block_size:]
+
             # Compute predictions
-            logits, _ = self(inputs)
+            logits, _ = self(inputs_cropped)
 
             # Take the last time step
             logits = logits[:, -1, :]
@@ -236,7 +354,6 @@ def get_optimal_device():
     Returns:
         torch.device
     """
-    return torch.device("cpu")
     if torch.cuda.is_available():
         return torch.device("cuda")
     elif torch.backends.mps.is_available():
