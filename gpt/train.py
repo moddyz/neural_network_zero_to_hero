4891f90907413df571f18ff24b828a8cb954c935
moddyz <moddy3d@gmail.com>
2023-12-27 15:16:04 +1300

    black fmt

--- gpt/train.py
+++ gpt/train.py
@@ -13,7 +13,6 @@ import matplotlib.pyplot as plt
 
 
 def main():
-
     with open("input.txt", "r", encoding="utf-8") as f:
         text = f.read()
 
@@ -36,7 +35,7 @@ def main():
     print(data.shape, data.dtype)
 
     # Separate data into training & validation sets.
-    train_data_size = int(0.9*len(data))
+    train_data_size = int(0.9 * len(data))
     train_data = data[:train_data_size]
     val_data = data[train_data_size:]
 
@@ -53,7 +52,9 @@ def main():
         device=get_optimal_device(),
     )
 
-    print(f"Starting training with batch size: {hyper_params.batch_size}, block size: {hyper_params.block_size}, number of training iterations: {hyper_params.train_iters}")
+    print(
+        f"Starting training with batch size: {hyper_params.batch_size}, block size: {hyper_params.block_size}, number of training iterations: {hyper_params.train_iters}"
+    )
 
     # Create the model
     model = BigramLanguageModel(vocab_size, hyper_params.device).to(hyper_params.device)
@@ -62,7 +63,6 @@ def main():
     optimizer = torch.optim.AdamW(model.parameters(), lr=hyper_params.learning_rate)
 
     for train_iter in range(hyper_params.train_iters):
-
         # Extract some training data.
         inputs, targets = get_batch(train_data, hyper_params)
 
@@ -70,11 +70,13 @@ def main():
         logits, loss = model(inputs, targets)
 
         # Estimate and print the optimization progress at every 1 percentage.
-        if train_iter % (hyper_params.train_iters/100) == 0:
+        if train_iter % (hyper_params.train_iters / 100) == 0:
             train_loss = estimate_loss(model, train_data, hyper_params)
             val_loss = estimate_loss(model, val_data, hyper_params)
             progress_percentage = float(train_iter) / hyper_params.train_iters * 100.0
-            print(f"progress: {progress_percentage:.01f}%, training iteration: {train_iter}/{hyper_params.train_iters}, training loss: {train_loss:.05f}, validation loss: {val_loss:.05f}")
+            print(
+                f"progress: {progress_percentage:.01f}%, training iteration: {train_iter}/{hyper_params.train_iters}, training loss: {train_loss:.05f}, validation loss: {val_loss:.05f}"
+            )
 
         # Zero out the gradients
         optimizer.zero_grad(set_to_none=True)
@@ -94,23 +96,25 @@ def main():
     print(string)
 
 
-HyperParameters = collections.namedtuple("HyperParameters", [
-    "batch_size", # Number of character sequences processed in parallel
-    "block_size", # Block size is the maximum context length to make predictions from.
-    "train_iters", # Number of iterations to train the model.
-    "eval_iters", # Number of iterations for estimating loss.
-    "learning_rate", # The learning rate
-    "device", # The device to store and execute our neutral network.
-])
+HyperParameters = collections.namedtuple(
+    "HyperParameters",
+    [
+        "batch_size",  # Number of character sequences processed in parallel
+        "block_size",  # Block size is the maximum context length to make predictions from.
+        "train_iters",  # Number of iterations to train the model.
+        "eval_iters",  # Number of iterations for estimating loss.
+        "learning_rate",  # The learning rate
+        "device",  # The device to store and execute our neutral network.
+    ],
+)
+
 
 class BigramLanguageModel(nn.Module):
-
     def __init__(self, vocab_size, device):
         super().__init__()
         self.token_embedding_table = nn.Embedding(vocab_size, vocab_size).to(device)
 
     def forward(self, inputs, targets=None):
-
         # This extracts a tensor with shape (Batch Size, Block Size, Vocab Size)
         # Where each integer from inputs corresponds to a vocab-sized embedding vector.
         logits = self.token_embedding_table(inputs)
@@ -128,7 +132,6 @@ class BigramLanguageModel(nn.Module):
         return logits, loss
 
     def generate(self, inputs, max_new_tokens):
-
         for _ in range(max_new_tokens):
             # Compute predictions
             logits, _ = self(inputs)
@@ -157,11 +160,17 @@ def get_batch(data, hyper_params):
     """
 
     # Generate random indices
-    random_indices = torch.randint(len(data) - (hyper_params.block_size + 1), (hyper_params.batch_size,))
+    random_indices = torch.randint(
+        len(data) - (hyper_params.block_size + 1), (hyper_params.batch_size,)
+    )
 
     # Extract rows of inputs and their corresponding targets.
-    inputs = torch.stack([data[i: i + hyper_params.block_size] for i in random_indices])
-    targets = torch.stack([data[i + 1: i + hyper_params.block_size + 1] for i in random_indices])
+    inputs = torch.stack(
+        [data[i : i + hyper_params.block_size] for i in random_indices]
+    )
+    targets = torch.stack(
+        [data[i + 1 : i + hyper_params.block_size + 1] for i in random_indices]
+    )
 
     # Upload data to specified device.
     inputs = inputs.to(hyper_params.device)
@@ -191,7 +200,6 @@ def estimate_loss(model, data, hyper_params):
 
     # Compute the loss multiple times.
     for eval_index in range(hyper_params.eval_iters):
-
         # Evaluate & store the loss for a batch
         inputs, targets = get_batch(data, hyper_params)
         _, loss = model(inputs, targets)
@@ -205,16 +213,19 @@ def estimate_loss(model, data, hyper_params):
 
 def visualize_weights_vs_gradients(parameters):
     # Visualize weight vs gradientlize histograms
-    plt.figure(figsize=(20, 4)) # width and height of the plot
+    plt.figure(figsize=(20, 4))  # width and height of the plot
     legends = []
     for i, p in enumerate(parameters):
         t = p.grad
-        print('weight %10s | mean %+f | std %e | grad:data ratio %e' % (tuple(p.shape), t.mean(), t.std(), t.std() / p.std()))
+        print(
+            "weight %10s | mean %+f | std %e | grad:data ratio %e"
+            % (tuple(p.shape), t.mean(), t.std(), t.std() / p.std())
+        )
         hy, hx = torch.histogram(t, density=True)
         plt.plot(hx[:-1].detach(), hy.detach())
-        legends.append(f'{i} {tuple(p.shape)}')
+        legends.append(f"{i} {tuple(p.shape)}")
         plt.legend(legends)
-        plt.title('weights gradient distribution');
+        plt.title("weights gradient distribution")
 
     plt.show()
 
@@ -235,5 +246,4 @@ def get_optimal_device():
 
 
 if __name__ == "__main__":
-
     main()
